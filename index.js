9b80a98d6034c237db09e9a80deb5aa3bbba0c94
chipchopchest <chip.chop.chest@gmail.com>
2024-04-27 18:07:59 +0200

    world

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
+const string = 'Hello world'
 // comment
-console.log('Hello world')
-// world
-// world
-// world
-// world
-// world
-// world
-// world
+console.log(string)
+
+alert(string)// h1Vo8+l1l1 Sat Apr 27 18:07:59 CEST 2024
