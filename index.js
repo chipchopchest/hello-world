05d0b2afe06814e7238e605936e0c6f87499e039
chipchopchest <chip.chop.chest@gmail.com>
2024-04-29 09:14:01 +0200

    world

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ const string = 'Hello world'
 console.log(string)
 
 alert(string)
-
+const onScrollStop = callback => {
+  let isScrolling;
+  window.addEventListener(
+    'scroll',
+    e => {
+      clearTimeout(isScrolling);
+      isScrolling = setTimeout(() => {
+        callback();
+      }, 150);
+    },
+    false
+  );
+};
