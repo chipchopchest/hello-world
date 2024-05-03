a802370544b9fe10e154bc7fef847f9495c9012a
chipchopchest <chip.chop.chest@gmail.com>
2024-05-03 07:40:00 +0200

    Fri May  3 07:40:00 CEST 2024

--- index.js
+++ index.js
@@ -38,3 +38,7 @@ const isRGBColor = (rgb) => {
   const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
   return regex.test(rgb);
 }
+const httpsRedirect = () => {
+  if (location.protocol !== 'https:')
+    location.replace('https://' + location.href.split('//')[1]);
+};
