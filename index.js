326263a8bb988a3615c42b684a847d2afa375e49
chipchopchest <chip.chop.chest@gmail.com>
2024-05-18 08:50:37 +0200

    Sat May 18 08:50:37 CEST 2024

--- index.js
+++ index.js
@@ -72,3 +72,14 @@ const isBitcoinAddress = (address) => {
   return regex.test(address);
 }
 let randomNum = Math.floor(Math.random() * maxNum);
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
