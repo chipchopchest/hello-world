3ad4215787cfad715db3ca0bc1a8e7d76e9d4d74
chipchopchest <chip.chop.chest@gmail.com>
2024-05-26 09:19:00 +0200

    Sun May 26 09:19:00 CEST 2024

--- index.js
+++ index.js
@@ -106,3 +106,6 @@ const isRGBColor = (rgb) => {
   const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
   return regex.test(rgb);
 }
+function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num) + "..." : str;
+}
