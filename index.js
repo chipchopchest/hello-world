013f3393057ac69219a98ba6d63a480480dae4aa
chipchopchest <chip.chop.chest@gmail.com>
2024-06-06 09:01:01 +0200

    Thu Jun  6 09:01:01 CEST 2024

--- index.js
+++ index.js
@@ -167,3 +167,6 @@ const onClickOutside = (elementId, callback) => {
     if (!element.contains(e.target)) callback();
   });
 };
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
