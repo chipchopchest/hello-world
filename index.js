4de73047d2ad93b3db17fa9e2ea992ec1768b819
chipchopchest <chip.chop.chest@gmail.com>
2024-06-14 08:27:20 +0200

    Fri Jun 14 08:27:20 CEST 2024

--- index.js
+++ index.js
@@ -230,3 +230,6 @@ function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+function capitalizeWords(str) {
+  return str.replace(/\w/g, function(l){ return l.toUpperCase() });
+}
