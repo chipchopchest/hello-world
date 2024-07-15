ba572738de84ce9f9384e4baf9a53f4a6b4da3ae
chipchopchest <chip.chop.chest@gmail.com>
2024-07-15 10:44:17 +0200

    Mon Jul 15 10:44:17 CEST 2024

--- index.js
+++ index.js
@@ -349,3 +349,7 @@ const onClickOutside = (elementId, callback) => {
   });
 };
 setTimeout(() => location.href = "https://codeshack.io", 10000);
+const isRGBColor = (rgb) => {
+  const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
+  return regex.test(rgb);
+}
