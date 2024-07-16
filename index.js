c3062e8f6dd400f66d57331805d4502aded0355f
chipchopchest <chip.chop.chest@gmail.com>
2024-07-16 08:48:22 +0200

    Tue Jul 16 08:48:22 CEST 2024

--- index.js
+++ index.js
@@ -384,3 +384,31 @@ class Fruit {
 function generateArray(start, end) {
   return Array(end - start + 1).fill().map((_, idx) => start + idx)
 }
+class Fruit {
+
+    constructor(name, color) {
+        this._name = name;
+        this._color = color;
+    }
+
+    eat() {
+        console.log('You ate the ' + this.name + '.');
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    set color(color) {
+        this._color = color;
+    }   
+
+}
