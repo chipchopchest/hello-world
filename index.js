86304efda2aeba28f05290a4c800004914296517
chipchopchest <chip.chop.chest@gmail.com>
2024-07-17 09:16:21 +0200

    Wed Jul 17 09:16:21 CEST 2024

--- index.js
+++ index.js
@@ -434,3 +434,8 @@ console.log(localStorage.getItem('username'));
 function sortByProperty(arr, property) {
   return arr.sort((a, b) => (a[property] > b[property]) ? 1 : -1);
 }
+window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth' 
+});
