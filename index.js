a4da27c11e0b2856a6069546dd1919bc87005c72
chipchopchest <chip.chop.chest@gmail.com>
2024-07-28 08:34:31 +0200

    Sun Jul 28 08:34:31 CEST 2024

--- index.js
+++ index.js
@@ -482,3 +482,4 @@ const isEthereumAddress = (address) => {
 function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+let fruit = fruits[Math.floor(Math.random()*fruits.length)];
