ce9dc448252c387f5fc5aaef1650f0f7220a24c7
chipchopchest <chip.chop.chest@gmail.com>
2024-07-31 07:09:28 +0200

    Wed Jul 31 07:09:27 CEST 2024

--- index.js
+++ index.js
@@ -499,3 +499,11 @@ const onScrollStop = callback => {
     false
   );
 };
+const btn = document.querySelector('a[data-btn]');
+btn.onclick = event => {
+    event.preventDefault();
+    // Output data attributes
+    console.log(btn.dataset.name); // David
+    console.log(btn.dataset.surname); // Adams
+    console.log(btn.dataset.uniqueId); // 30
+};
