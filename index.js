df67aed3061c54ecb9747e28181b98895afd2cad
chipchopchest <chip.chop.chest@gmail.com>
2024-08-03 08:30:32 +0200

    Sat Aug  3 08:30:32 CEST 2024

--- index.js
+++ index.js
@@ -512,3 +512,7 @@ const isCreditCard = (cc) => {
   const regex = /(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/;
   return regex.test(cc);
 }
+const isBitcoinAddress = (address) => {
+  const regex = /[13][a-km-zA-HJ-NP-Z0-9]{25,34}/;
+  return regex.test(address);
+}
