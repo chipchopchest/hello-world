4776e6564f3d2ffe3b3b634058c09bc0b8a010aa
chipchopchest <chip.chop.chest@gmail.com>
2024-09-01 15:32:02 +0200

    Sun Sep  1 15:32:02 CEST 2024

--- index.js
+++ index.js
@@ -648,3 +648,14 @@ function reverseString(str) {
 if (localStorage.getItem('username')) {
     console.log('Item exists!');
 }
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
